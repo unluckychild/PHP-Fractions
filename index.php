e2d27604b6d0fb8af953cb70b9e0d71d2d1bc930
unluckychild <94160802+unluckychild@users.noreply.github.com>
2022-12-22 15:53:26 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include ("class.FractionMath.php");
+
+$gg=new FractionMath();
+
+$first="1 2/3";
+$second="2 2/3";
+echo "$first + $second = ";
+
+
+//plus, minus, multiply, divide;
+$gg->plus($first,$second);
+
+echo $gg->getResult() ."<br><br>";
+$gg->toMixed();
+echo "to mixed not reduced: ".$gg->getResult() ."<br><br>";
+echo "convert to fractions again: ".$gg->toFractions($gg->result) ."<br><br>";
+$gg->reduceFraction();
+echo "fractions reduced: ".$gg->getResult() ."<br><br>";
+$gg->toMixed();
+echo "from reduced to mixed: ".$gg->getResult() ."<br><br>";
+echo "convert to fractions again: ".$gg->toFractions($gg->result);
+
+echo "<br><br>";
+echo "Whole number: ".$gg->getwhole() ." Fractions: " . $gg->getFractions();
+
+?>
